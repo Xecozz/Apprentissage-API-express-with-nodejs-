4341d9eb4a6aaf2c747bdfc0ddd1c7b7dfba4275
Xecozz <94707887+Xecozz@users.noreply.github.com>
2024-10-06 11:46:01 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,60 @@
-const express = require('express'); //module
+const express = require("express"); //module
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const colors = require('colors');
-const Thing = require('./models/Thing');
-const { collection } = require('./models/Thing');
+const mongoose = require("mongoose");
+const colors = require("colors");
+const Thing = require("./models/Thing");
+const { collection } = require("./models/Thing");
 
-const app = express()//initialization
+const app = express(); //initialization
 
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser())
+app.use(bodyParser());
 //connecter à la bdd
-mongoose.connect('mongodb+srv://camille:Cg260205@cluster0.pbedp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect("url", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-  
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 //inscription
-app.use('/api/inscription', (req, res, next)=> {
+app.use("/api/inscription", (req, res, next) => {
   const thing = new Thing({
-    ...req.body
+    ...req.body,
   });
-  thing.save()
-    .then(()=> res.status(201).redirect('http://localhost:5500/frontend/account/login.html'))
-    .catch(error => res.status(400).json({error}));
-  console.log(thing)
+  thing
+    .save()
+    .then(() =>
+      res
+        .status(201)
+        .redirect("http://localhost:5500/frontend/account/login.html")
+    )
+    .catch((error) => res.status(400).json({ error }));
+  console.log(thing);
 });
 
 //login
-app.use('/api/login', (req, res, next)=> {
-  Thing.findOne({ mail : req.body.maillogin, password : req.body.passwordlogin})
-    .then(thing => res.redirect(`http://127.0.0.1:5500/frontend/nasa/index.html`))
-    .catch(error => res.status(400).json({ message : 'impossible de ce connecter'}));
-
+app.use("/api/login", (req, res, next) => {
+  Thing.findOne({ mail: req.body.maillogin, password: req.body.passwordlogin })
+    .then((thing) =>
+      res.redirect(`http://127.0.0.1:5500/frontend/nasa/index.html`)
+    )
+    .catch((error) =>
+      res.status(400).json({ message: "impossible de ce connecter" })
+    );
 });
 
-  module.exports = app;
+module.exports = app;
--- readme.md
+++ readme.md
@@ -1,3 +1,59 @@
-Nodejs
-Express
-MongoDB
+# Projet "Une Date, Une Image"
+
+Ce projet est une application web interactive qui permet aux utilisateurs de découvrir une image liée à l'espace en entrant une date spécifique. Le projet utilise Node.js, Express, et MongoDB pour le backend, et HTML, CSS, et JavaScript pour le frontend.
+
+## Structure du projet
+
+## Prérequis
+
+- Node.js
+- npm (Node Package Manager)
+- MongoDB
+
+## Installation
+
+1. Clonez le dépôt :
+
+   ```sh
+   git clone <URL_DU_DEPOT>
+   ```
+
+2. Installez les dépendances pour le backend :
+
+   ```sh
+   cd backend
+   npm install
+   ```
+
+3. Installez les dépendances pour le frontend :
+
+   ```sh
+   cd ../frontend
+   npm install
+   ```
+
+## Utilisation
+
+1. Démarrez le serveur backend :
+
+   ```sh
+   cd backend
+   npm start
+   ```
+
+2. Ouvrez le fichier `index.html` dans le dossier `frontend/nasa` dans votre navigateur pour accéder à l'application.
+
+## Dépendances
+
+### Backend
+
+- [Express](https://www.npmjs.com/package/express)
+- [Mongoose](https://www.npmjs.com/package/mongoose)
+- [Body-parser](https://www.npmjs.com/package/body-parser)
+- [Colors](https://www.npmjs.com/package/colors)
+
+### Frontend
+
+- HTML
+- CSS
+- JavaScript
